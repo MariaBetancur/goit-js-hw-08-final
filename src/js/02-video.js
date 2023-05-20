b6ec0ddd202efed7ab63ddbee889b1e46d121f85
MariaBetancur <mabo6241@gmail.com>
2023-05-19 19:17:52 -0500

    v1.4

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,12 @@
-const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+import Player from '@vimeo/player';
+
+const player = new Player('handstick', {
+  id: 19231868,
+  width: 640,
+});
 
 player.on('play', function () {
   console.log('played the video!');
-});
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
 });
 const onPlay = function (data) {
   // data is an object containing properties specific to that event
